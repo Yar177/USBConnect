1a3ddac167935a84726b7aca767bfe8d5759f9a9
Yar Mohammad <10190270@kodakalaris.com>
2018-06-19 13:40:27 -0400

    Initial commit

---
+++ app/src/main/java/com/example/android/usbconnect/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.android.usbconnect;
+
+import android.content.Intent;
+import android.hardware.usb.UsbDevice;
+import android.hardware.usb.UsbManager;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = (TextView) findViewById(R.id.textView);
+
+        Intent intent = new Intent();
+
+        UsbDevice device = (UsbDevice) intent.getParcelableExtra(UsbManager.EXTRA_DEVICE);
+
+        if (device != null){
+            textView.setText("Camera Attached");
+        }
+
+
+    }
+}
